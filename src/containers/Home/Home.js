1206502f6e5355141f92ecd55148e03fd159f667
dongshaoyang <dongshaoyang@baidu.com>
2017-08-07 16:46:58 +0800

    demo

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import User from './User';
 import Header from './components/Header.js';
 import Content from './components/Content.js';
 import './Home.less';
@@ -9,6 +10,8 @@ export default class Home extends Component {
       <div id="pageHome">
         <Header />
         <Content />
+        <User name="Andy" desc="大家好1"/>
+        <User name="Bob" desc="大家好2"/>
       </div>
     );
   }
